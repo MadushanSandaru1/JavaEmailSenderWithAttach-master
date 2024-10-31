3314ba61779acec842a46a1709af7703678089a1
Nishara Kavindi <kavindi183@gmail.com>
2024-10-31 11:42:07 +0530

    Update JavaEmailSenderWithAttach.java
    Add log message

--- src/javaemailsenderwithattach/JavaEmailSenderWithAttach.java
+++ src/javaemailsenderwithattach/JavaEmailSenderWithAttach.java
@@ -15,6 +15,7 @@ public class JavaEmailSenderWithAttach {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("===JavaEmailSenderWithAttach Started===");
         new MainFrame().setVisible(true);
     }
     
